21ee9c0a93fd731abd6fb5ae19703d40063dbbea
DaryaZubkova <dashazub@inbox.ru>
2025-04-26 13:34:03 +0300

    fix: lesson_12

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        ArrayProcessor processor = new ArrayProcessor();
+
         String[][] correctArray = {
                 {"1", "2", "3", "4"},
                 {"5", "6", "7", "8"},
@@ -18,23 +20,22 @@ public class Main {
                 {"7", "8", "9"},
                 {"10", "11", "12"}
         };
+        try {
+            System.out.println("Сумма correctArray: " + processor.processArray(correctArray));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+        try {
+            System.out.println("Сумма invalidDataArray: " + processor.processArray(invalidDataArray));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+        try {
+            System.out.println("Сумма wrongSizeArray: " + processor.processArray(wrongSizeArray));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
 
-        try {
-            System.out.println("Сумма correctArray: " + lesson_12.ArrayProcessor.processArray(correctArray));
-        } catch (lesson_12.MyArraySizeException | lesson_12.MyArrayDataException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
-        try {
-            System.out.println("Сумма invalidDataArray: " + lesson_12.ArrayProcessor.processArray(invalidDataArray));
-        } catch (lesson_12.MyArraySizeException | lesson_12.MyArrayDataException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
-        try {
-            System.out.println("Сумма wrongSizeArray: " + lesson_12.ArrayProcessor.processArray(wrongSizeArray));
-        } catch (lesson_12.MyArraySizeException | lesson_12.MyArrayDataException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
-
-        lesson_12.ArrayProcessor.demonstrateIndexOutOfBounds();
+        processor.demonstrateIndexOutOfBounds();
     }
 }
